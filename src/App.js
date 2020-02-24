bb1fef412bf92df291aa1299650f205f8db97b6d
tim <timothy.potts@colorado.edu>
2020-02-24 08:30:09 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Wrapper from "./components/Wrapper/";
+import Header from "./components/Header";
+import Table from "./components/Table";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Table />
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
